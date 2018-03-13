957c8dafcf84139bd3ea63ddba7f427f9ac9381f
noPEx <soumyamandi@gmail.com>
2018-03-14 02:48:22 +0530

    4 types experiment

--- threaded_metrics.py
+++ threaded_metrics.py
@@ -12,7 +12,7 @@ class my_timer:
 
     def __exit__(self, type, value, traceback):
         self.end = time.time()
-        print 'exit ::', (self.end - self.start)
+        print self.name, 'took ==> ',(self.end - self.start)
 
 class CountThread(Thread):
 
@@ -23,13 +23,94 @@ class CountThread(Thread):
 
     def run(self):
         print 'curr thread', threading.currentThread()
-        with my_timer(self.name):
-            while self.N>0:
-                self.N -= 1
+        while self.N>0:
+            self.N -= 1
 
-N = 100*100*1000
-COUNT *= 10
 
-th1 = CountThread(N, 'thread1')
-#th1.start()
-th1.run()
+def one_thread_only(N):
+    print '1 thread working'
+    name = 'one_thread_only' 
+    with my_timer(name):
+        th1 = CountThread(N, name)
+        th1.start()
+        th1.join()
+
+def one_func_only(N):
+    print 'main thread function only'
+    name = 'one_func_only' 
+    with my_timer(name):
+        th1 = CountThread(N, 'one_func_only')
+        th1.run()
+    
+
+def use_two_thread(N):
+    print '2 threads each counting half'
+    name = 'use_two_thread'
+    N = N/2
+
+    with my_timer(name):
+    
+        th1 = CountThread(N, 'use_two_thread_1')
+        th2 = CountThread(N, 'use_two_thread_2')
+        th1.start()
+        th2.start()
+        th1.join()
+        th2.join()
+
+def use_two_func(N):
+    print 'main thread call two functions each counting half'
+    name = 'use_two_func'
+
+    N = N/2
+    with my_timer(name):
+    
+        th1 = CountThread(N, 'thread1')
+        th2 = CountThread(N, 'thread2')
+        th1.run()
+        th2.run()
+
+def use_four_thread(N):
+    print '4 thread workers counting 1/4th'
+    name = 'use_four_thread'
+
+    N = N/4
+    with my_timer(name):
+        th1 = CountThread(N, 'thread1')
+        th2 = CountThread(N, 'thread2')
+        th3 = CountThread(N, 'thread3')
+        th4 = CountThread(N, 'thread4')
+        th1.start()
+        th2.start()
+        th3.start()
+        th4.start()
+        th1.join()
+        th2.join()
+        th3.join()
+        th4.join()
+
+
+
+
+def main():
+
+    N = 100*100*1000*10
+
+    one_thread_only(N) 
+    print '\n\n'
+    one_func_only(N)
+    print '\n\n'
+    use_two_thread(N) 
+    print '\n\n'
+    use_two_func(N)
+    print '\n\n'
+    use_four_thread(N)
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
